027c611342b0719368d113dbdf777c8147b7e010
Amanda Milke <amandalmilke@gmail.com>
2016-02-10 19:00:12 -0500

    second commit

--- script.js
+++ script.js
@@ -10,20 +10,43 @@ var groceryList = [
 	} 
 ];
 
-var total = 0;
+//Function for Adding Grocery List to index.html
+var total=0; //sets initial total counter equal to zero
 
 groceryList.forEach(function(item) {
-	console.log(item.groceryItem + "- \$" + item.price);
-	total += item.price;
+	var unorderedList = document.getElementById('shoppinglist'); //creates a variable for the unordered list with the ID 'shoppinglist' in the HTML
+	var myList = document.createElement('li'); //creates list items for each grocery item & price, creates a variable of it to call later
+	var displayTotal = document.getElementById('totalDisplay'); //creates a variable for the grocery item total from the <p> tag in the HTML
+
+	myList.innerHTML=item.groceryItem + "- \$" + item.price; //adds the grocery items and prices into the html in 'li' format
+	unorderedList.appendChild(myList); //adds the li to the unordered list
+	
+	total += item.price; //calculates the total price, will run through each loop
 });
 
-console.log("$" + total);
+	document.getElementById('totalDisplay').innerHTML = "Total: $" + total; //displays the total in the paragraph after the unordered list
 
-// var total = 0;
 
-// for (var i = 0; i < groceryList.length; i++) {
-// 	console.log(groceryList[i].groceryItem + "- \$" + groceryList[i].price);
-// 	total += groceryList[i].price;
-// }
+//BONUS: Where It All Goes Wrong
 
-// 	console.log("$" + total);
+ 	function submitNewStuff() {
+ 	var itemAdd = document.getElementById("itemInput").value; //naming an added item to a variable
+ 	var priceAdd = document.getElementById("priceInput").value; //naming an added price to a variable
+
+
+	var unorderedList = document.getElementById('shoppinglist'); //creates a variable for the unordered list with the ID 'shoppinglist' in the HTML
+	var myList = document.createElement('li'); //creates list items for each grocery item & price, creates a variable of it to call later
+	var displayTotal = document.getElementById('totalDisplay'); //creates a variable for the grocery item total from the <p> tag in the HTML
+
+ 	priceAdd = parseInt(priceAdd); //making sure the price is a number
+
+	myList.innerHTML=itemAdd + "- \$" + priceAdd; //adds the grocery items and prices into the html in 'li' format
+	unorderedList.appendChild(myList); //adds the li to the unordered list
+
+
+ 	total += priceAdd;
+ }
+
+ 	// var adding=document.getElementById('submitNewStuff');
+ 	// adding.addEventListener("click", submitNewStuff, false);
+
